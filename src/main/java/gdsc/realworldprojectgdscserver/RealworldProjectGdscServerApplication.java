5c72185d46677cfe6436ca0022eb31937bb3fdb4
Octoping <myc1365@naver.com>
2024-01-15 23:58:21 +0900

    Initial commit

---
+++ src/main/java/gdsc/realworldprojectgdscserver/RealworldProjectGdscServerApplication.java
@@ -0,0 +1,13 @@
+package gdsc.realworldprojectgdscserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealworldProjectGdscServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealworldProjectGdscServerApplication.class, args);
+    }
+
+}
