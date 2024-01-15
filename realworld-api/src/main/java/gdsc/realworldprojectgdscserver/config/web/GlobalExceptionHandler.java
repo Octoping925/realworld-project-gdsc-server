1faa2d6694fe0afd80716d7fdc78ac4f87cf2c83
Octoping <myc1365@naver.com>
2024-01-16 03:17:03 +0900

    feat: GlobalExceptionHandler 생성

---
+++ realworld-api/src/main/java/gdsc/realworldprojectgdscserver/config/web/GlobalExceptionHandler.java
@@ -0,0 +1,8 @@
+package gdsc.realworldprojectgdscserver.config.web;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    
+}
