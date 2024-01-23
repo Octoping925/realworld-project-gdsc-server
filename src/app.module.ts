e8c058367fba2b16a334dc580e2bdbc42dabc349
Octoping <myc1365@naver.com>
2024-01-24 01:02:27 +0900

    feat: User 리소스 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { ArticleModule } from './article/article.module';
 import { CommentModule } from './comment/comment.module';
 import { FavoriteModule } from './favorite/favorite.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [ArticleModule, CommentModule, FavoriteModule],
+  imports: [ArticleModule, CommentModule, FavoriteModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
