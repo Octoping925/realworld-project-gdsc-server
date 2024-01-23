876f880202dc13d7d03d5e96eef0006383d2d348
Octoping <myc1365@naver.com>
2024-01-24 01:02:05 +0900

    feat: Favorite 리소스 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ArticleModule } from './article/article.module';
 import { CommentModule } from './comment/comment.module';
+import { FavoriteModule } from './favorite/favorite.module';
 
 @Module({
-  imports: [ArticleModule, CommentModule],
+  imports: [ArticleModule, CommentModule, FavoriteModule],
   controllers: [AppController],
   providers: [AppService],
 })
