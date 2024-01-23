b63acec0ca9150ff0d4172461b27b525abb014ee
Octoping <myc1365@naver.com>
2024-01-24 02:14:08 +0900

    feat: follow 모듈의 api 엔드포인트와 더미데이터 추가

--- src/follow/follow.controller.ts
+++ src/follow/follow.controller.ts
@@ -1,34 +1,27 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Delete, Param, Post } from '@nestjs/common';
 import { FollowService } from './follow.service';
-import { CreateFollowDto } from './dto/create-follow.dto';
-import { UpdateFollowDto } from './dto/update-follow.dto';
+import { ProfileDto } from '../user/dto/profile.dto';
+import { DUMMY_PROFILE } from '../user/schema/profile.dummy.schema';
 
-@Controller('follow')
+@Controller('profiles/:username/follow')
 export class FollowController {
   constructor(private readonly followService: FollowService) {}
 
   @Post()
-  create(@Body() createFollowDto: CreateFollowDto) {
-    return this.followService.create(createFollowDto);
+  public followUser(@Param('username') username: string): ProfileDto {
+    // this.followService.create(+id);
+
+    return {
+      profile: DUMMY_PROFILE,
+    };
   }
 
-  @Get()
-  findAll() {
-    return this.followService.findAll();
-  }
+  @Delete()
+  public unfollowUser(@Param('username') username: string): ProfileDto {
+    // this.followService.remove(+username);
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.followService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateFollowDto: UpdateFollowDto) {
-    return this.followService.update(+id, updateFollowDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.followService.remove(+id);
+    return {
+      profile: DUMMY_PROFILE,
+    };
   }
 }
