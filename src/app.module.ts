52a4d2f3d89cbb268ca50f587fac1e4d2cadf323
Octoping <myc1365@naver.com>
2024-01-24 01:04:18 +0900

    feat: Follow 리소스 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { CommentModule } from './comment/comment.module';
 import { FavoriteModule } from './favorite/favorite.module';
 import { UserModule } from './user/user.module';
 import { TagModule } from './tag/tag.module';
+import { FollowModule } from './follow/follow.module';
 
 @Module({
-  imports: [ArticleModule, CommentModule, FavoriteModule, UserModule, TagModule],
+  imports: [ArticleModule, CommentModule, FavoriteModule, UserModule, TagModule, FollowModule],
   controllers: [AppController],
   providers: [AppService],
 })
