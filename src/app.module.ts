f8a4d11448b0b2424ddb78f56262ed5807f8bc01
Octoping <myc1365@naver.com>
2024-01-24 01:01:43 +0900

    feat: Comment 리소스 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ArticleModule } from './article/article.module';
+import { CommentModule } from './comment/comment.module';
 
 @Module({
-  imports: [ArticleModule],
+  imports: [ArticleModule, CommentModule],
   controllers: [AppController],
   providers: [AppService],
 })
