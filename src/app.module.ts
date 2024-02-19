1d2dcdaaea6f84773803c3aa68a5911ceef84a7d
Octoping <mooner925@gmail.com>
2024-02-19 19:12:45 +0900

    feat: TypeORM 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,28 @@ import { TagModule } from './domain/tag/tag.module';
 import { FollowModule } from './domain/follow/follow.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config(
+  process.env.NODE_ENV === 'production'
+    ? { path: '.env' }
+    : { path: '.env.development' },
+);
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: 5432,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+      ssl: true,
+    }),
     ArticleModule,
     CommentModule,
     FavoriteModule,
--- src/domain/user/entities/user.entity.ts
+++ src/domain/user/entities/user.entity.ts
@@ -1,1 +1,20 @@
-export class User {}
+import { Column, Entity, Generated, PrimaryColumn } from 'typeorm';
+
+@Entity('user')
+export class User {
+  @PrimaryColumn()
+  @Generated('')
+  id: number;
+
+  @Column({ length: 30 })
+  email: string;
+
+  @Column({ length: 30 })
+  username: string;
+
+  @Column({ length: 100, nullable: true })
+  bio: string | null;
+
+  @Column({ length: 100, nullable: true })
+  image: string | null;
+}
